0b207ac0c94b120d66ced78570d15fe4c3efef37
Jetaime Alexandria <rondellivey03@gmail.com>
2024-03-22 17:44:46 -0400

    Made changes to fit the new Weapons page instead.

--- scripts/Monoforge.js
+++ scripts/Monoforge.js
@@ -1,19 +1,19 @@
-let weapons, tools, metal_ingots, equipment;
+let weapons;
 
 function WeaponTypeCSS(type) {
   switch (type) {
     case "Primary":
-      return "bg-orange-900 hover:bg-orange-700 outline outline-3 outline-orange-900";
+      return "bg-orange-500 hover:bg-orange-600 dark:bg-orange-900 dark:hover:bg-orange-700 outline outline-3 outline-orange-500 dark:outline-orange-900";
       break;
     case "Secondary":
-      return "bg-blue-900 hover:bg-blue-700 outline outline-3 outline-blue-900";
+      return "bg-blue-500 hover:bg-blue-600 dark:bg-blue-900 dark:hover:bg-blue-700 outline outline-3 outline-blue-500 dark:outline-blue-900"; 
       break;
     case "Melee":
-      return "bg-red-900 hover:bg-red-700 outline outline-3 outline-red-900";
+      return "bg-red-500 hover:bg-red-700 dark:bg-red-900 dark:hover:bg-red-600 outline outline-3 outline-red-500 dark:outline-red-900";
       break;
 
     default:
-      return "bg-gray-600 hover:bg-gray-500 outline outline-3 outline-gray-600";
+      return "bg-gray-500 hover:bg-gray-600 outline outline-3 outline-gray-700";
       break;
   }
 }
@@ -23,14 +23,20 @@ weapons = fetch("data/Weapons.json")
   .then((data) => {
     data[0].section_items.forEach((element) => {
       let item_html = `
-        <li class="p-2 m-2 bg-gray-300 dark:bg-gray-700 hover:bg-gray-400 dark:hover:bg-gray-600  items-center space-x-4">
-         <img src="./assets/Weapons/${element.unique_id}.png" alt="${element.name}" class="lines p-2 w-16 h-16 ${WeaponTypeCSS(element.type)}">
-         <span class="text-dark dark:text-gray-300 font-bold text-xl"><b>${element.name}</b><span class="text-sm float-right">Req: ${element.skill} ${element.level}</span></span>
-        </li>`;
+        <div class="bg-gray-300 dark:bg-gray-600 dark:hover:bg-gray-500 hover:bg-gray-400 rounded-lg p-6 flex items-center justify-center space-x-4 cursor-pointer" data-weapon-type="${element.type}">
+         <img src="./assets/Weapons/${element.unique_id}.png" alt="${element.name}" class="lines p-2 w-16 h-16 drop-on-death ${WeaponTypeCSS(element.type)}">
+         <h2 class="text-2xl font-bold my-4">${element.name}</h2>
+        </div>`;
 
       document
-        .querySelector("#items")
+        .querySelector("#weapon-list")
         .insertAdjacentHTML("beforeend", item_html);
     });
   })
   .catch((error) => console.error(error));
+
+document.querySelectorAll(".MFSelectors").forEach((element) => {
+  element.addEventListener("click", () => {
+    
+  });
+})
