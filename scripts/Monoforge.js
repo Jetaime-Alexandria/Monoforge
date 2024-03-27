150f44e0626bb6216bcc7cff362047a4e642bab2
Jetaime Alexandria <rondellivey03@gmail.com>
2024-03-26 21:05:50 -0400

    Made a function to individually fetch each of the JSON files.

--- scripts/Monoforge.js
+++ scripts/Monoforge.js
@@ -38,33 +38,83 @@ function typeCSS(type) {
 let amount = 0;
 let weaponList;
 
-fetch("data/Weapons.json")
-  .then((response) => response.json())
-  .then((data) => {
-    weaponList = data[0].section_items;
-    data[0].section_items.forEach((element) => {
-      let item_html = `
-      <div class="bg-gray-200 dark:bg-gray-800 rounded-lg p-6 mb-4 text-gray-200 dark:text-gray-300 items-center text-center ${typeCSS(element.type)}" data-type="${element.type}" data-name="${element.name}" data-unique-id="${element.unique_id}">
-        <h2 class="text-2xl font-bold mb-4 text-center">${element.name}</h2>
-        <img src="/assets/Weapons/${element.unique_id}.png" alt="${element.name}" class="w-32 h-32 mx-auto mb-4">
-        <span class="text-xl font-bold mb-4 text-center">Lvl: ${element.skill} ${element.level}</span>
-        </div>`;
+async function loadItems(){
+  
+await fetch("data/Weapons.json")
+.then((response) => response.json())
+.then((data) => {
+  weaponList = data[0].section_items;
+  data[0].section_items.forEach((element) => {
+    let item_html = `
+    <div class="bg-gray-200 dark:bg-gray-800 rounded-lg p-6 mb-4 text-gray-200 dark:text-gray-300 items-center text-center ${typeCSS(element.type)}" data-type="${element.type}" data-name="${element.name}" data-unique-id="${element.unique_id}">
+      <h2 class="text-2xl font-bold mb-4 text-center">${element.name}</h2>
+      <img src="/assets/Weapons/${element.unique_id}.png" alt="${element.name}" class="w-32 h-32 mx-auto mb-4">
+      <span class="text-xl font-bold mb-4 text-center">Lvl: ${element.skill} ${element.level}</span>
+      </div>`;
 
-      document.querySelector("#result-list").insertAdjacentHTML("beforeend", item_html);
+    document.querySelector("#result-list").insertAdjacentHTML("beforeend", item_html);
 
-      document.querySelector(`[data-unique-id="${element.unique_id}"]`).addEventListener("click", function () {
-        window.location.href = `./view-item.html?id=${element.unique_id}`;
-      });
-
-      amount++;
-      document.getElementById("result-count").innerHTML = amount;
+    document.querySelector(`[data-unique-id="${element.unique_id}"]`).addEventListener("click", function () {
+      window.location.href = `./view-item.html?id=${element.unique_id}`;
     });
-  })
-  .catch((error) => console.error(error));
 
+    amount++;
+    document.getElementById("result-count").innerHTML = amount;
+  });
+})
+.catch((error) => console.error(error));
 
+await fetch("data/Attachments.json")
+.then((response) => response.json())
+.then((data) => {
+  weaponList = data[0].section_items;
+  data[0].section_items.forEach((element) => {
+    let item_html = `
+    <div class="bg-gray-200 dark:bg-gray-800 rounded-lg p-6 mb-4 text-gray-200 dark:text-gray-300 items-center text-center ${typeCSS()}" data-type="Attachments" data-name="${element.name}" data-unique-id="${element.unique_id}">
+      <h2 class="text-2xl font-bold mb-4 text-center">${element.name}</h2>
+      <img src="/assets/attachment.png" alt="${element.name}" class="w-32 h-32 mx-auto mb-4">
+      <span class="text-xl font-bold mb-4 text-center">Lvl: ${element.skill} ${element.level}</span>
+      </div>`;
 
-document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector("#result-list").insertAdjacentHTML("beforeend", item_html);
+
+    document.querySelector(`[data-unique-id="${element.unique_id}"]`).addEventListener("click", function () {
+      window.location.href = `./view-item.html?id=${element.unique_id}`;
+    });
+
+    amount++;
+    document.getElementById("result-count").innerHTML = amount;
+  });
+})
+.catch((error) => console.error(error));
+
+await fetch("data/Ammo.json")
+.then((response) => response.json())
+.then((data) => {
+  weaponList = data[0].section_items;
+  data[0].section_items.forEach((element) => {
+    let item_html = `
+    <div class="bg-gray-200 dark:bg-gray-800 rounded-lg p-6 mb-4 text-gray-200 dark:text-gray-300 items-center text-center ${typeCSS()}" data-type="Weapon Ammo" data-name="${element.name}" data-unique-id="${element.unique_id}">
+      <h2 class="text-2xl font-bold mb-4 text-center">${element.name}</h2>
+      <img src="/assets/ammo/${element.unique_id}.png" alt="${element.name}" class="w-32 h-32 mx-auto mb-4">
+      <span class="text-xl font-bold mb-4 text-center">Lvl: ${element.skill} ${element.level}</span>
+      </div>`;
+
+    document.querySelector("#result-list").insertAdjacentHTML("beforeend", item_html);
+
+    document.querySelector(`[data-unique-id="${element.unique_id}"]`).addEventListener("click", function () {
+      window.location.href = `./view-item.html?id=${element.unique_id}`;
+    });
+
+    amount++;
+    document.getElementById("result-count").innerHTML = amount;
+  });
+})
+.catch((error) => console.error(error));
+
+}
+
+document.addEventListener("DOMContentLoaded", async function () {
   const categoryCheckboxes = document.querySelectorAll(".category");
   const searchBar = document.querySelector("#search");
 
@@ -103,5 +153,16 @@ document.addEventListener("DOMContentLoaded", function () {
     checkbox.addEventListener("change", filterWeapons);
   });
  
+  await loadItems();
   filterWeapons();
+
+  $("#check-all").on("click", function () {
+    $(".category").prop("checked", true);
+    filterWeapons();
+  });
+
+  $("#uncheck-all").on("click", function () {
+    $(".category").prop("checked", false);
+    filterWeapons();
+  });
 });
